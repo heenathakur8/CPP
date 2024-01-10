0fe24d8ddc9f0c8ab1724cc8c6aa1707b32b3b90
heenathakur8 <heena01thakur@gmail.com>
2024-01-10 17:04:36 +0530

    using Binary Search

---
+++ minpagesBS.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool countStudents(int ar[],int m,int n,int c)
+{
+	int cnt=1,s=0;
+
+	for(int i=0;i<n;i++)
+	{
+		if(ar[i]+s<=m)
+			s+=ar[i];
+		else
+		{
+			cnt++;
+	    	s=ar[i];
+		}
+	}
+	return cnt>c;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int ar[n],sum=0,mn=INT_MAX;
+	
+	for(int i=0;i<n;i++)
+	{
+		cin>>ar[i];
+		mn=min(mn,ar[i]);
+		sum+=ar[i];
+	}
+	
+	int st;
+	cin>>st;
+
+	int l=mn,h=sum,mid=(l+h)/2;
+
+	while(l<=h)
+	{
+		mid=(l+h)/2;
+		if(countStudents(ar,mid,n,st))
+			l=mid+1;		
+		else
+			h=mid-1;
+			
+	}
+
+	cout<<l;
+
+}
